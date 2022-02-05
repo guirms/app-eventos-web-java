545a6877a0795e9d302613e0fd6ab69aef80a628
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-02-05 00:29:36 -0300

    Lista de eventos em /eventos

--- src/main/java/com/guirms/eventoApp/controllers/EventoController.java
+++ src/main/java/com/guirms/eventoApp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.guirms.eventoApp.models.Evento;
 import com.guirms.eventoApp.repositories.eventoRepository;
@@ -23,6 +24,15 @@ public class EventoController {
 	public String eventoForm(Evento evento) {
 		eventoRepo.save(evento);
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping(value = "/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepo.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+		
 	}
 	
 	
